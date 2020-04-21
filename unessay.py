d6321aed01080cc8276d69c6c311bebcb7b29552
pauldan24 <32079319+pauldan24@users.noreply.github.com>
2020-04-21 17:30:05 -0600

    certs and keys for https

--- unessay.py
+++ unessay.py
@@ -28,7 +28,9 @@ def index():
 
 #main
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(ssl_context=('cert.pem', 'key.pem'))
+    # app.run(debug=True)
+    # app.run(ssl_context='adhoc')
 
 
 
